3f7d107f6b39d4d9c8e244882036ae1638405f41
AlexKomanov <akomanov88@gmail.com>
2023-11-03 10:46:04 +0200

    Adding 11_string_f.py

---
+++ lesson_05_strings/11_string_f.py
@@ -0,0 +1,10 @@
+name = "alex"
+last_name = 'komanov'
+age = 35
+
+# print("My name is " + name + ". \nLast name is " + last_name + ". \nMy age is " + str(age))
+print(f"My name is {name.upper()} .Last name is {last_name.upper()}. My age is {age}. \nNext year I'll be {age + 1}")
+
+print(f"{name.upper() = }")
+print(f"{last_name = }")
+
