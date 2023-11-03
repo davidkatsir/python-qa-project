22fc35689529baa54a2472edd3e981efe01068bb
AlexKomanov <akomanov88@gmail.com>
2023-11-03 10:45:46 +0200

    Adding 09_string_camparison.py

---
+++ lesson_05_strings/09_string_camparison.py
@@ -0,0 +1,9 @@
+name1 = "Alex"
+name2 = "Alex"
+name3 = "alex"
+
+print(name1 == name2)
+print(name1 == name3)
+print(name1.lower() == name3.lower())
+print(name1.upper() == name3.upper())
+print(name1 != name3)
