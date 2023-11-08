1eb09fcfb0cbf41948fefeab258c03129bb9dad2
AlexKomanov <akomanov88@gmail.com>
2023-11-08 17:18:35 +0200

    Adding class tasks

---
+++ lesson_06_conditional_statements/class_tasks/02_solution.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a first number: "))
+number_2 = int(input("Enter a second number: "))
+
+if number > number_2:
+    print(number)
+else:
+    print(number_2)
+
+print(number) if number > number_2 else print(number_2)
